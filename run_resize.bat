bab3ba34d4503ab01c04be63e1e590c77b9c3761
AmriTSalahudin <amri.salahudin@gmail.com>
2025-11-25 08:37:58 +0700

    hehehe selesai revisi

---
+++ run_resize.bat
@@ -0,0 +1,4 @@
+@echo off
+REM rirektori tersetah kalian ah,ojo opo opo aku opo opo aku. kesel aku yo jon
+cd /d "D:\hehehe\resizeappNODEJS" 
+powershell -NoProfile -ExecutionPolicy Bypass -Command "npm run resize -- --in-place --width 1200 --quality 80 --min-size-kb 500"
